b47a641629e23c0a43cabc127a2ccba6c3047782
David Karauni <davymwachits@gmail.com>
2024-07-29 16:49:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+// Function to update employee with a new key-value pair (non-destructive)
+const employee = {
+  name: "John Doe",
+  streetAddress: "123 Main St"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+     ...employee,
+      [key]: value
+    };
+  }
+  
+  // Function to update employee with a new key-value pair (destructive)
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete a key from employee (non-destructive)
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to delete a key from employee (destructive)
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  module.exports = {
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey
+  };
+  
